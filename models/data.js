12054c894904a5049ad898ffca6f4218e871f168
Beny <Beny@Emilys-MacBook-Air.local>
2022-11-23 11:46:14 -0600

    done with project

--- models/data.js
+++ models/data.js
@@ -17,7 +17,7 @@ module.exports = [
         model: "GTR",
         hp: "600",
         description: " The Nissan GT-R (Japanese: 日産・GT-R, Nissan GT-R), is a high-performance sports car and grand tourer produced by Nissan, unveiled in 2007.[3][4][5] It is the successor to the Skyline GT-R, a high performance variant of the Nissan Skyline. Although this car was the sixth-generation model to bear the GT-R name, the model is no longer part of the Nissan Skyline model line up since that name is now reserved for Nissan's luxury-sport vehicles. The GT-R is built on the exclusively-developed Nissan PM platform, which is an enhanced evolution of the Nissan FM platform used in the separate Nissan Skyline luxury car and the Nissan Z sports car. The GT-R abbreviation stands for Gran Turismo-Racing, obtained from the Skyline GT-R.",
-        video:"https://www.youtube.com/embed/bT7ka-NtCFw"
+        video:"https://www.youtube.com/embed/BUeq9nn0pzU"
     },
     {
         name: "EVO",
@@ -27,7 +27,7 @@ module.exports = [
         model: "Lancer Evolution",
         hp: "286",
         description: "The Mitsubishi Lancer Evo IX has a turbocharged four cylinders in line transverse front engine providing a maximum torque of 355 Nm available from 3500 rpm and a maximum power output of 280 PS available at 6500 rpm transmitted to the 17 inch permanent all-wheel drive by a manual 5 speed gearbox",
-        video:"https://www.youtube.com/embed/bT7ka-NtCFw"
+        video:"https://www.youtube.com/embed/cus7J2Ry4-8"
     },
     {
         name: "180-SX",
@@ -37,7 +37,7 @@ module.exports = [
         model: "180-sx",
         hp: "140 hp/TRASH",
         description: "The Nissan 180SX is a fastback automobile that was produced by Nissan Motors between 1988 and 1998. It is based on the S13 chassis from the Nissan S platform with the variants receiving an R designation (ex. PS13 and RPS13), and was sold exclusively in Japan paired with the CA18 motor in the early models; later models paired with the SR20 motor. Outside of Japan, it was re-badged as the 200SX[1] and in the US market as the Nissan 240SX, paired with the single overhead cam KA24E motor and later with the dual-overhead model KA24DE.",
-        video:"https://www.youtube.com/embed/bT7ka-NtCFw"
+        video:"https://www.youtube.com/embed/xL3fEWf1xyM"
     },
     {
         name: "RX-7",
@@ -47,7 +47,7 @@ module.exports = [
         model: "RX-7",
         hp: "255",
         description: "The Mazda RX-7 is a front-engine, rear-wheel-drive, rotary engine-powered sports car that was manufactured and marketed by Mazda from 1978 until 2002 across three generations, all of which made use of a compact, lightweight Wankel rotary engine.",
-        video:"https://www.youtube.com/embed/bT7ka-NtCFw"
+        video:"https://www.youtube.com/embed/FnGM4ymYjxY"
     },
     {
         name: "PORSCHE 911",
@@ -57,7 +57,7 @@ module.exports = [
         model: "911",
         hp: "something",
         description: "The 911 traces its roots to sketches drawn by Ferdinand 'Butzi' Porsche in 1959.The Porsche 911 was developed as a more powerful, larger and a more comfortable replacement for the 356, the company's first model. The new car made its public debut at the 1963 Frankfurt Motor Show[8] (German: Internationale Automobil-Ausstellung).[9] The car was developed with the proof-of-concept twin-fan Type 745 flat-six engine, but the car presented at the auto show had a non-operational mockup of the single-fan 901 engine, receiving a working unit in February 1964.",
-        video:"https://www.youtube.com/embed/bT7ka-NtCFw"
+        video:"https://www.youtube.com/embed/4Gd_t5Va8-s"
     },
     {
         name: "Silvia S15",
@@ -67,7 +67,7 @@ module.exports = [
         model: "Silvia 200SX S15",
         hp: "250",
         description: "The S15 Silvia included aggressive styling inside and out; upgrades to the chassis and suspension, including increased use of light alloy suspension components; and for the first time, a six-speed manual transmission (in addition to a 5-speed manual transmission and a four-speed automatic). Also for the first time, instead of a viscous limited slip differential, the S15 offered a more effective helical limited slip unit.",
-        video:"https://www.youtube.com/embed/bT7ka-NtCFw"
+        video:"//www.youtube.com/embed/GW3mBgxdsCU"
     },
     {
         name: "C6 Corvette",
@@ -77,7 +77,7 @@ module.exports = [
         model: "C6 Corvette",
         hp: "430",
         description: "The Chevrolet Corvette (C6) is the sixth generation of the Corvette sports car that was produced by Chevrolet division of General Motors for the 2005 to 2013 model years. It is the first Corvette with exposed headlamps since the 1962 model. Production variants include the Z06, ZR1, Grand Sport, and 427 Convertible. Racing variants include the C6.R, an American Le Mans Series GT1 championship and 24 Hours of Le Mans GTE-Pro winner.",
-        video:"https://www.youtube.com/embed/bT7ka-NtCFw"
+        video:"https://www.youtube.com/embed/E9_Iu2Sr2bc"
     },
     {
         name: "Viper SRT GTS",
@@ -87,7 +87,7 @@ module.exports = [
         model: "Viper SRT GTS",
         hp: "640",
         description: "The Dodge Viper is a sports car that was manufactured by Dodge (by SRT for 2013 and 2014), a division of American car manufacturer FCA US LLC from 1992 until 2017, having taken a brief hiatus in 2007 and from 2010 to 2012. Production of the two-seat super car began at New Mack Assembly Plant in 1991 and moved to Conner Avenue Assembly Plant in October 1995.",
-        video:"https://www.youtube.com/embed/bT7ka-NtCFw"
+        video:"https://www.youtube.com/embed/uKfv-LA0CyY"
     },
     {
         name: "Acura NSX",
@@ -97,7 +97,7 @@ module.exports = [
         model: "NSX",
         hp: "270",
         description: "The NSX project was initiated in 1984. The engineers at Tochigi Research and Development Center created a list of attributes the future NSX had to possess. First on their list was top-rank performance on a level equal to or greater than the existing exotics. Their interpretation of performance was not narrowly defined to a few areas such as power, handling and brakillg, but included areas that are often given less attention by the traditional builders of exotica.",
-        video:"https://www.youtube.com/embed/bT7ka-NtCFw"
+        video:"https://www.youtube.com/embed/q5PPNZiu52w"
     },
 
 ]
